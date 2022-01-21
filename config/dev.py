e37e14667d446d1c6499554584ea827792360458
luis-munayco <luis.munayco@pucp.edu.pe>
2022-01-21 11:33:09 -0500

    development

--- config/dev.py
+++ config/dev.py
@@ -5,4 +5,4 @@ from .default import *
 
 APP_ENV = APP_ENV_DEVELOPMENT
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:NewPass@localhost/bmeswebapp'
