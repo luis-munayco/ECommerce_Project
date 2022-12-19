18c81c587a83ce3d333cfcb763ff2430473dc094
Luis M <95502258+luis-munayco@users.noreply.github.com>
2022-12-19 19:29:46 +0100

    Update default.py

--- config/default.py
+++ config/default.py
@@ -6,7 +6,7 @@ from os.path import abspath, dirname
 # Define the application directory
 BASE_DIR = dirname(dirname(abspath(__file__)))
 
-SECRET_KEY = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+SECRET_KEY = ''
 
 # Database configuration
 SQLALCHEMY_TRACK_MODIFICATIONS = False
